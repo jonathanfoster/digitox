d1b6120c7708b6cb3c7892ccc0f1bb42793f5a86
Jonathan Foster <jonathan.e.foster@gmail.com>
2018-05-13 11:29:26 -0400

    Fix casing of version JSON payload

--- server/handlers/version.go
+++ server/handlers/version.go
@@ -7,11 +7,14 @@ import (
 // ServerVersion is the API server version.
 var ServerVersion string
 
+// VersionPayload is the payload returned from the GET / route.
+type VersionPayload struct {
+	Version string `json:"version"`
+}
+
 // Version handles the GET / route.
 func Version(w http.ResponseWriter, r *http.Request) {
-	JSON(w, http.StatusOK, struct {
-		Version string
-	}{
+	JSON(w, http.StatusOK, &VersionPayload{
 		Version: ServerVersion,
 	})
 }
